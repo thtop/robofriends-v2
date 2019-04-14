b5c7869edb78b4108f9a7a006d378a58ca4aa813
Thamonwan Maneechan <47081950+thtop@users.noreply.github.com>
2019-04-14 14:59:52 +0700

    RoboFriends V2
    - APIs
    - ErrorBoundry

---
+++ build/precache-manifest.5959dad4b8398778fad572c063cb339b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e372a2153f7759c1b3ea",
+    "url": "/static/css/main.e59d589b.chunk.css"
+  },
+  {
+    "revision": "e372a2153f7759c1b3ea",
+    "url": "/static/js/main.c3db6098.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a037b93a2c1e4e27953",
+    "url": "/static/css/2.294af5e7.chunk.css"
+  },
+  {
+    "revision": "2a037b93a2c1e4e27953",
+    "url": "/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "26efc6e7eca4bdf85044ae83eac58a6b",
+    "url": "/index.html"
+  }
+];
---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBroundry from '../components/ErrorBoundry';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users').then(response => response.json())
+        .then(users => this.setState({ robots: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState( { searchfield: event.target.value} );
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase());
+        })
+
+        return !robots.length ?
+            <h1 className='tc'>Loading</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <ErrorBroundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBroundry>
+                    </Scroll>
+                </div>
+            );
+        
+    }
+}
+
+export default App;
